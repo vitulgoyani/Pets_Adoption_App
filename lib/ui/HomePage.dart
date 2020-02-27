8300cd36059c568f727faf2200ba1def34c16e2b
Umesh Lakhani <umeshlakhani7170@gmail.com>
2020-02-27 19:24:48 +0530

    - design home page

--- lib/ui/HomePage.dart
+++ lib/ui/HomePage.dart
@@ -8,13 +8,13 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final GlobalKey<InnerDrawerState> _innerDrawerKey =
-      GlobalKey<InnerDrawerState>();
+  GlobalKey<InnerDrawerState>();
   bool _swipe = true;
   InnerDrawerAnimation _animationType = InnerDrawerAnimation.static;
   bool _proportionalChildArea = true;
   double _verticalOffset = 0.0;
   bool _topBottom = false;
-  double _scale = 0.7;
+  double _scale = 0.9;
   double _borderRadius = 50;
 
   @override
@@ -22,7 +22,7 @@ class _HomePageState extends State<HomePage> {
     print(MediaQuery.of(context).size.width);
     Color iconColor = Color(0xff86b3b3);
     TextStyle menuText = TextStyle(
-        color: Color(0xff86b3b3), fontSize: 24, fontWeight: FontWeight.bold);
+        color: Color(0xff86b3b3), fontSize: 24, fontWeight: FontWeight.w500);
     return InnerDrawer(
         backgroundColor: Color(0xff497473),
         key: _innerDrawerKey,
@@ -30,8 +30,8 @@ class _HomePageState extends State<HomePage> {
         offset: IDOffset.only(
             top: _topBottom ? _verticalOffset : 0.0,
             bottom: !_topBottom ? _verticalOffset : 0.0,
-            right: MediaQuery.of(context).size.width<450?0.4:0,
-            left: MediaQuery.of(context).size.width<450?0.4:0),
+            right: MediaQuery.of(context).size.width < 450 ? 0.4 : 0,
+            left: MediaQuery.of(context).size.width < 450 ? 0.4 : 0),
         scale: IDOffset.horizontal(_scale),
         borderRadius: _borderRadius,
         duration: Duration(milliseconds: 11200),
@@ -50,7 +50,7 @@ class _HomePageState extends State<HomePage> {
                   ListTile(
                     leading: CircleAvatar(
                       backgroundImage:
-                          AssetImage("assets/adult-1867889_640.jpg"),
+                      AssetImage("assets/adult-1867889_640.jpg"),
                     ),
                     title: Text(
                       "Adoption",
@@ -154,9 +154,157 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
         scaffold: Scaffold(
-          appBar: AppBar(
-            title: Text("This is title"),
+          body: Container(
+            child: SafeArea(
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      IconButton(
+                          icon: Icon(Icons.menu),
+                          onPressed: () {
+                            _innerDrawerKey.currentState.toggle();
+                          }),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "Location",
+                            style: TextStyle(fontSize: 14),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Icon(
+                                Icons.location_on,
+                                color: Color(0xff497473),
+                              ),
+                              SizedBox(
+                                width: 2,
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    "Surat,",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ),
+                                  Text(
+                                    "India",
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: CircleAvatar(
+                          backgroundImage:
+                          AssetImage("assets/adult-1867889_640.jpg"),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.05),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                        ),
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 30),
+                            child: Container(
+                                width: MediaQuery.of(context).size.width,
+                                decoration: BoxDecoration(
+                                    borderRadius:
+                                    BorderRadius.all(Radius.circular(15)),
+                                    color: Colors.white),
+                                child: Padding(
+                                  padding:
+                                  const EdgeInsets.symmetric(horizontal: 8),
+                                  child: TextFormField(
+                                    cursorColor: Theme.of(context).accentColor,
+                                    decoration: InputDecoration(
+                                      prefixIcon: Icon(Icons.search),
+                                      hintText: "Search",
+                                      border: InputBorder.none,
+                                      suffixIcon: Icon(Icons.sort),
+                                    ),
+                                  ),
+                                )),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 120,
+                            child: ListView(
+                              itemExtent: 80,
+                              scrollDirection: Axis.horizontal,
+                              shrinkWrap: true,
+                              children: <Widget>[
+                                categoryBox(),
+                                categoryBox(),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ));
+  }
+
+  Column categoryBox() {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 60,
+          width: 60,
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                    color: Color(0xff497473).withOpacity(0.3),
+                    offset: Offset(0, 5),
+                    blurRadius: 10,
+                    spreadRadius: 5)
+              ],
+              color: Color(0xff497473),
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Image.asset(
+              "assets/cat.png",
+              color: Colors.white,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text("Cats")
+      ],
+    );
   }
 }
